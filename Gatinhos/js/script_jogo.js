140fd5330b139f93349c7034d782cbd7f0767bdb
LizzBricce <141193412+LizzBricce@users.noreply.github.com>
2023-11-15 23:27:51 -0300

    Update script_jogo.js, liz 15/09 23:27

--- Gatinhos/js/script_jogo.js
+++ Gatinhos/js/script_jogo.js
@@ -1,96 +1,281 @@
 class Node {
-    constructor(data) {
+  constructor(data) {
       this.data = data;
       this.next = null;
-    }
   }
-  
-  class LinkedList {
-    constructor() {
+}
+
+class LinkedList {
+  constructor() {
       this.head = null;
-    }
-  
-    // Função para inserir um novo elemento no final da lista
-    insert(data) {
+  }
+
+  insert(data) {
       const newNode = new Node(data);
       if (!this.head) {
-        this.head = newNode;
+          this.head = newNode;
       } else {
-        let current = this.head;
-        while (current.next) {
-          current = current.next;
-        }
-        current.next = newNode;
+          let current = this.head;
+          while (current.next) {
+              current = current.next;
+          }
+          current.next = newNode;
       }
-    }
-  
-    // Função para remover um elemento da lista
-    remove(data) {
+  }
+
+
+  remove(data) {
       if (!this.head) {
-        return;
+          return;
       }
       if (this.head.data === data) {
-        this.head = this.head.next;
-        return;
+          this.head = this.head.next;
+          return;
       }
       let current = this.head;
       let previous = null;
       while (current && current.data !== data) {
-        previous = current;
-        current = current.next;
+          previous = current;
+          current = current.next;
       }
       if (current) {
-        previous.next = current.next;
+          previous.next = current.next;
       }
-    }
-  
-    // Função para exibir todos os elementos da lista
-    displayAll() {
+  }
+
+
+  displayAll() {
       let current = this.head;
       while (current) {
-        console.log(current.data);
-        current = current.next;
+          console.log(current.data);
+          current = current.next;
       }
+  }
+}
+
+
+
+const img = document.querySelector(".container-imagem");
+
+const divFalas = document.querySelector(".falas");
+const elementosDeTexto = divFalas.querySelectorAll("h1");
+const listaNomesClicados = new LinkedList();
+
+const cenarios = [
+  {
+      img: "img/cenario1.png",
+      falas: "qual é sua atividade preferida em grupo?",
+      respostabotao1: "show",
+      respostabotao2: "videogame",
+      respostabotao3: "piquenique",
+      respostabotao4: "exposição de arte"
+  },
+  {
+      img: "img/cenario1.png",
+      falas: "qual palavra melhor descreve seu estilo de musica favorito?",
+      respostabotao1: "eletronica",
+      respostabotao2: "classica",
+      respostabotao3: "alternativa",
+      respostabotao4: "oeck"
+  },
+  {
+    img: "img/cenario1.png",
+    falas: "o que mais te inspira?",
+    respostabotao1: "autenticidade",
+    respostabotao2: "tecnologia",
+    respostabotao3: "beleza",
+    respostabotao4: "sensibilidade"
+},
+{
+    img: "img/cenario1.png",
+    falas: "se você tivesse que escolher uma cor qual seria?",
+    respostabotao1: "preto",
+    respostabotao2: "azul",
+    respostabotao3: "branco",
+    respostabotao4: "vermelho"
+},
+{
+    img: "img/cenario1.png",
+    falas: "qual palavra melhor descreve sua atitude em relação a regras?",
+    respostabotao1: "desafiador",
+    respostabotao2: "flexivel",
+    respostabotao3: "respeitoso",
+    respostabotao4: "criativo"
+},
+{
+    img: "img/cenario1.png",
+    falas: "como você reage a situaçôes estressantes?",
+    respostabotao1: "distraido",
+    respostabotao2: "calmo",
+    respostabotao3: "reflexivo",
+    respostabotao4: "ignorante"
+}
+  
+      ];
+      function atualizarTela(indice) {
+    const cena = cenarios[indice];
+    
+    // Atualiza a imagem
+    const img = document.querySelector(".container-img img");
+    img.src = cena.img;
+
+    // Atualiza o texto da fala
+    elementosDeTexto[0].textContent = cena.falas;
+    
+    // Atualiza os botões com as respostas correspondentes
+    const botoes = document.querySelectorAll(".botaoRespostas");
+    for (let i = 0; i < botoes.length; i++) {
+        botoes[i].textContent = cena[`respostabotao${i + 1}`];
     }
-  }
-  
-  // Seleciona todos os botões com a classe "botaoRespostas"
-  const falas = ["ai que nao sei oq nao sei oq la",
-   "*** ******* ******* ",
-    "uiuiuiuiu ", "Texto 4", 
-    ""];
-  let indiceFalaAtual = 0;
-  
-  // Seleciona a div com a classe "falas"
-  const divFalas = document.querySelector(".falas");
-  
-  // Seleciona todos os elementos h1 dentro da div
-  const elementosDeTexto = divFalas.querySelectorAll("h1");
-  
-  // Crie uma lista encadeada para armazenar os nomes dos botões clicados
-  const listaNomesClicados = new LinkedList();
-  
-  // Atualiza o texto da fala atual
-  function atualizarFala() {
-      elementosDeTexto[0].textContent = falas[indiceFalaAtual];
-  }
-  
-  // Adiciona um ouvinte de evento de clique para cada botão
-  const botoes = document.querySelectorAll(".botaoRespostas");
-  botoes.forEach(function(botao) {
-      botao.addEventListener("click", function() {
-          // Obtém o valor do atributo "data-nome" do botão clicado
-          const dataNome = botao.getAttribute("data-nome");
-          listaNomesClicados.insert(dataNome); // Adiciona o nome do botão à lista encadeada
-          console.log("Nomes dos botões clicados:");
-          listaNomesClicados.displayAll(); // Exibe todos os nomes dos botões clicados na lista
-          indiceFalaAtual++;
-          if (indiceFalaAtual >= falas.length) {
-              indiceFalaAtual = 0;
-          }
-          atualizarFala();
-      });
-  });
-  
-  // Atualiza o texto da fala inicial
-  atualizarFala();
+}
+
+    
+ // Adiciona um ouvinte de evento de clique para cada botão
+const botoes = document.querySelectorAll(".botaoRespostas");
+let indiceCenaAtual = 0;
+
+
+// Função para lidar com o clique em um botão
+function handleButtonClick(dataNome) {
+    listaNomesClicados.insert(dataNome);
+    indiceCenaAtual++;
+
+    if (indiceCenaAtual < cenarios.length) {
+        atualizarTela(indiceCenaAtual);
+    } else {
+        exibirResultadoFinal();
+       
+    }
+}
+
+
+// Atualiza a tela inicial
+atualizarTela(indiceCenaAtual);
+
+// Adiciona o ouvinte de evento de clique a cada botão
+botoes.forEach(function (botao) {
+    botao.addEventListener("click", function () {
+        const cenaAtual = cenarios[indiceCenaAtual];
+        const dataNome = botao.getAttribute("data-nome");
+        handleButtonClick(dataNome);
+        
+    });
+});
+
+function compararlistas() {
+    const listaGatinho1 = new LinkedList();
+    const listaGatinho2 = new LinkedList();
+    const listaGatinho3 = new LinkedList();
+
+   
+    listaGatinho1.insert("1resposta");
+    listaGatinho1.insert("2resposta");
+    listaGatinho1.insert("1resposta");
+    listaGatinho1.insert("2resposta"); 
+    listaGatinho1.insert("1resposta");
+    
+
+    listaGatinho2.insert("1resposta");
+    listaGatinho2.insert("3resposta");
+    listaGatinho2.insert("4resposta");
+    listaGatinho2.insert("4resposta");
+    listaGatinho2.insert("1resposta");
+
+    listaGatinho3.insert("2resposta");
+    listaGatinho3.insert("3resposta");
+    listaGatinho3.insert("4resposta");
+    listaGatinho3.insert("4resposta");
+    listaGatinho3.insert("3resposta");
+    // ...
+
+    // Compare o conteúdo de cada listaGatinho com listaNomesClicados e atribua pontuações
+    const pontuacaoGatinho1 = calcularPontuacao(listaGatinho1, listaNomesClicados);
+    const pontuacaoGatinho2 = calcularPontuacao(listaGatinho2, listaNomesClicados);
+    const pontuacaoGatinho3 = calcularPontuacao(listaGatinho3, listaNomesClicados);
+
+    // Determine qual listaGatinho tem a pontuação mais alta
+    if (pontuacaoGatinho1 > pontuacaoGatinho2 && pontuacaoGatinho1 > pontuacaoGatinho3) {
+        gatinhoFinal = "lysandre";
+    } else if (pontuacaoGatinho2 > pontuacaoGatinho1 && pontuacaoGatinho2 > pontuacaoGatinho3) {
+        gatinhoFinal = "armin";
+    } else {
+        gatinhoFinal = "castiel";
+    }
+}
+
+// Função para calcular a pontuação comparando o conteúdo de duas listas encadeadas
+function calcularPontuacao(listaGatinho, listaComparada) {
+    let atualGatinho = listaGatinho.head;
+    let atualComparada = listaComparada.head;
+    let pontuacao = 0;
+    
+
+    while (atualGatinho && atualComparada) {
+        if (atualGatinho.data === atualComparada.data) {
+            pontuacao++;
+        }
+        atualGatinho = atualGatinho.next;
+        atualComparada = atualComparada.next;
+    }
+
+    return pontuacao;
+}
+let gatinhoFinal;
+
+function exibirResultadoFinal() {
+    console.log("Fim do jogo!");
+    console.log("Itens da lista encadeada:");
+    listaNomesClicados.displayAll();
+
+    // Adiciona chamada para compararlistas após o final do jogo
+    compararlistas();
+    console.log("Gatinho Final:", gatinhoFinal);
+
+    // Oculta os elementos de texto e a caixa de texto
+    document.querySelector('.falas').style.display = 'none';
+    document.getElementById('caixaDeTexto1').style.display = 'none';
+    document.querySelector('.botoes').style.display = 'none';
+
+    // Exibe o resultado final na div do cenário
+    const cenaDiv = document.querySelector('.container-img');
+
+    
+
+    // Div para o nome do gatinho
+    const nomeGatinhoResultadoDiv = document.createElement('h2');
+    nomeGatinhoResultadoDiv.classList.add('nomeGatinhoh2', 'iniciarAnimacao');
+    nomeGatinhoResultadoDiv.textContent = `Gatinho Final: ${gatinhoFinal}`;
+    
+    // Div para o botão "Ver Gatinho"
+    const botaoVerGatinhoDiv = document.createElement('div');
+    botaoVerGatinhoDiv.classList.add('botaoVerGatinhoResultado');
+    botaoVerGatinhoDiv.innerHTML = `<button onclick="verGatinho()" src ="img/caixa.png" id="verGatinhoBtn">ver gatinho</button>`;
+    
+    // Adiciona as divs à cenaDiv
+    cenaDiv.appendChild(nomeGatinhoResultadoDiv);
+    cenaDiv.appendChild(botaoVerGatinhoDiv);
+
+    // Desabilita o botão após o primeiro clique
+    const verGatinhoBtn = document.getElementById('verGatinhoBtn');
+    verGatinhoBtn.addEventListener('click', function() {
+        verGatinho();
+        verGatinhoBtn.disabled = true;
+    });
+
+    // Adiciona a classe que inicia a animação após todos os elementos serem criados
+    nomeGatinhoResultadoDiv.classList.add('iniciarAnimacao');
+}
+
+function verGatinho() {
+    // Adicione lógica para exibir a imagem do gatinho correspondente ao resultado final
+    const gatinhoResultadoDiv = document.createElement('div');
+    gatinhoResultadoDiv.classList.add('.imagemGatinhos');
+
+    // Use a variável gatinhoFinal para escolher a imagem correta
+    const imagemGatinho = `img/${gatinhoFinal}.png`;
+    
+
+    gatinhoResultadoDiv.innerHTML = `<img class="gatito" src="${imagemGatinho}" alt="Gatinho Resultado">`;
+    document.body.appendChild(gatinhoResultadoDiv);
+    
+};
